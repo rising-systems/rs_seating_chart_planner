02f1625151fd745162d8371764e99b0e0be4426a
RS-BA55 <69146031+RS-BA55@users.noreply.github.com>
2025-07-01 09:52:17 +0200

    Revert "fixed svg scaling on large screens; added profile picture scaling; ad…"

--- rs_seating_chart_planner/models/extends_rs_location.py
+++ rs_seating_chart_planner/models/extends_rs_location.py
@@ -1,9 +1,7 @@
 from odoo import fields, models, api
 
-
 class ExtendsRsLocation(models.Model):
     _inherit = 'rs.location'
 
     svg_image = fields.Binary(string='SVG File')
-    seat_assignments = fields.One2many(
-        'rs.location.seat.assignment', 'location_id', string='Seat Assignments')
+    seat_assignments = fields.One2many('rs.location.seat.assignment', 'location_id', string='Seat Assignments')
