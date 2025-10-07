5b0beab392c0b99f726b3b02ffae6d592838208e
unknown <fernandez@rising-systems.de>
2025-10-07 11:21:25 +0200

    Hide already added users to seating plan in the multiple add window

--- rs_seating_chart_planner/models/rs_location_add_users_wizard.py
+++ rs_seating_chart_planner/models/rs_location_add_users_wizard.py
@@ -6,6 +6,24 @@ class RsLocationAddUsersWizard(models.TransientModel):
 
     location_id = fields.Many2one('rs.location', string='Location', required=True)
     user_ids = fields.Many2many('res.users', string='Users to Add', required=True, help='Select multiple users to add as seat assignments')
+    available_user_ids = fields.Many2many('res.users', string='Available Users', compute='_compute_available_users', store=False)
+
+    @api.depends('location_id')
+    def _compute_available_users(self):
+        """Compute available users excluding those already assigned to this location"""
+        for wizard in self:
+            if wizard.location_id:
+                # Get users already assigned to this location
+                assigned_user_ids = wizard.location_id.seat_assignments.mapped('user_id.id')
+
+                # Get all users except the already assigned ones
+                available_users = self.env['res.users'].search([
+                    ('id', 'not in', assigned_user_ids),
+                    ('share', '=', False)  # Only internal users
+                ])
+                wizard.available_user_ids = available_users
+            else:
+                wizard.available_user_ids = self.env['res.users']
 
     def action_add_users(self):
         """Add selected users as seat assignments to the location"""
